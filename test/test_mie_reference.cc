0168e010bb87fa805811777359e2cd77b456ffe2
codesaurus97 <marcip97@gmail.com>
2022-04-16 15:15:41 +0200

    Wiscombe test values added

--- test/test_mie_reference.cc
+++ test/test_mie_reference.cc
@@ -9,6 +9,11 @@ class MieScatteringTestSet : public ::testing::TestWithParam<std::tuple<complex<
   double dummy;
 };
 
+class MieScatteringWiscombe : public ::testing::TestWithParam<std::tuple<complex<double>, double, double>> {
+ public:
+  double dummy;
+};
+
 static double ApplyPrecision(const double& value, int precision)
 {
     const auto exponent = static_cast<int>(std::floor(std::log10(value)))+1;
@@ -48,8 +53,22 @@ TEST_P(MieScatteringTestSet, QbackQext_baseline)
     cppmie::MieScatteringBaseLine(x, m, qext, qsca, qback);
 
     // test for 6 value precisions (NOT DECIMALS)
-    ASSERT_FLOAT_EQ(ApplyPrecision(qext,6), qext_gold);
-    ASSERT_FLOAT_EQ(ApplyPrecision(qsca, 6), qsca_gold);
+    ASSERT_DOUBLE_EQ(ApplyPrecision(qext,6), qext_gold);
+    ASSERT_DOUBLE_EQ(ApplyPrecision(qsca, 6), qsca_gold);
+}
+
+TEST_P(MieScatteringWiscombe, QbackWiscombe)  // Wiscombe test for backscattering
+{
+    const auto m = std::get<0>(GetParam());
+    const auto x = std::get<1>(GetParam());
+    const auto qback_wiscombe = std::get<2>(GetParam());
+
+    double qext;
+    double qsca;
+    double qback;
+    cppmie::MieScatteringBaseLine(x, m, qext, qsca, qback);
+
+    ASSERT_DOUBLE_EQ(qback, qback_wiscombe);
 }
 
 INSTANTIATE_TEST_SUITE_P(
@@ -90,3 +109,58 @@ INSTANTIATE_TEST_SUITE_P(
         std::make_tuple(complex<double>{10., 10.}, 100., 2.07112, 1.83679),
         std::make_tuple(complex<double>{10., 10.}, 10000., 2.00591, 1.79539)
     ));
+
+// Wiscombe Tests for Mie Scattering Qback
+// Source for Wiscombe Test Values: https://miepython.readthedocs.io/en/latest/09_backscattering.html
+INSTANTIATE_TEST_SUITE_P(
+    WiscombePerfectlyConducting,
+    MieScatteringWiscombe,
+    ::testing::Values(
+        std::make_tuple(complex<double>{1.5500, 0.0}, 5.213, 2.925340e+00),
+        std::make_tuple(complex<double>{0.0000, -1000.00}, 0.099, 8.630064e-04),
+        std::make_tuple(complex<double>(0.0000 -1000.00), 0.101, 9.347732e-04),
+        std::make_tuple(complex<double>(0.0000 -1000.00), 100., 9.990256e-01),
+        std::make_tuple(complex<double>(0.0000 -1000.00), 10000., 9.999997e-01)));
+
+INSTANTIATE_TEST_SUITE_P(
+    SmallerRefractiveThanEnvironment,
+    MieScatteringWiscombe,
+    ::testing::Values(
+        std::make_tuple(complex<double>{0.75, 0.0}, 0.099, 1.108554e-05),
+        std::make_tuple(complex<double>{0.75, 0.0}, 0.101, 1.200382e-05),
+        std::make_tuple(complex<double>(0.75, 0.0), 10.000, 4.658462e-02),
+        std::make_tuple(complex<double>(0.75, 0.0), 1000.000, 9.391600e-01)));
+
+INSTANTIATE_TEST_SUITE_P(
+    NonAbsorbing,
+    MieScatteringWiscombe,
+    ::testing::Values(
+        std::make_tuple(complex<double>{1.5, 0.0}, 10., 1.695084e+00),
+        std::make_tuple(complex<double>{1.5, 0.0}, 100., 1.736102e+00),
+        std::make_tuple(complex<double>(1.5, 0.0), 1000., 1.030182e+01)));
+
+INSTANTIATE_TEST_SUITE_P(
+    WaterDroplets,
+    MieScatteringWiscombe,
+    ::testing::Values(
+        std::make_tuple(complex<double>{1.3300, -0.00001}, 10., 8.462494e-02),
+        std::make_tuple(complex<double>{1.3300, -0.00001}, 100., 2.146327e+00),
+        std::make_tuple(complex<double>(1.3300, -0.00001), 1000., 3.757215e-02)));
+
+INSTANTIATE_TEST_SUITE_P(
+    ModeratelyAbsorbingSpheres,
+    MieScatteringWiscombe,
+    ::testing::Values(
+        std::make_tuple(complex<double>{1.5, -1.}, 0.055, 1.695493e-05),
+        std::make_tuple(complex<double>{1.5, -1.}, 0.056, 1.822197e-05),
+        std::make_tuple(complex<double>{1.5, -1.}, 1., 5.730036e-01),
+        std::make_tuple(complex<double>{1.5, -1.}, 100., 1.724214e-01),
+        std::make_tuple(complex<double>(1.5, -1.), 1000., 1.724138e-01)));
+
+INSTANTIATE_TEST_SUITE_P(
+    BigRefraction,
+    MieScatteringWiscombe,
+    ::testing::Values(
+        std::make_tuple(complex<double>{10., -10.}, 1., 3.308998e+00),
+        std::make_tuple(complex<double>{10., -10.}, 100., 8.201267e-01),
+        std::make_tuple(complex<double>(10., -10.), 1000., 8.190052e-01)));
